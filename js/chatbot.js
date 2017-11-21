15f002070b7a89dcfed52c6a400e7ca1dad40c6d
Taras Kulyk <tk@div-art.com>
2017-11-21 15:32:40 +0200

    v 1.0

--- js/chatbot.js
+++ js/chatbot.js
@@ -36,12 +36,22 @@ function Chatbot(item) {
         return (h + ':' + m);
     };
     
+    self.handleText = function(text) {
+        let username = self.username || '';
+        let useremail = self.useremail || '';
+        if (text.type == 'text') {
+            text.text = text.text.replace('{{username}}', username);
+            text.text = text.text.replace('{{useremail}}', useremail);
+        }
+        return text;
+    };
+    
     self.renderQuesMessage = function(message) {
         let time = self.getTime();
         let html = '';
         for (let i in message['texts']) {
             let classContainer = ((i*1 +1) === message['texts'].length);
-            let text = message['texts'][i];
+            let text = self.handleText(message['texts'][i]);
             html += `
                 <div class="hu-message brand animate left-in">
                     <div class="hu-message-container `+(classContainer ? 'active' : '') +`">
@@ -62,7 +72,7 @@ function Chatbot(item) {
                                 case 'image': 
                                     html += `
                                         <div class="hu-message-image" style="display: inline-block; opacity: 1;">
-                                            <img src="https://storage.googleapis.com/media.helloumi.com/channels/0_gL8rmnA.gif">
+                                            <img src="`+text.src+`">
                                         </div>
                                     `; 
                             }
@@ -72,8 +82,7 @@ function Chatbot(item) {
                         </div>
                     </div>
                 </div>
-                            `;
-                            
+            `;
         }
         return html;
     };
@@ -85,15 +94,40 @@ function Chatbot(item) {
             <div class="hu-input-box" style="height: auto;">
                 <div class="hu-input-container menu">
                     <div class="hu-input-header"></div>
-                    <div class="hu-input-body">
-                        <div class="hu-input-menu"><span class="hu-input-menu-info not-selectable hu-s-10 hu-color_contrast hu-t-uppercase">Choose an option</span>
-                            <div class="hu-input-menu-buttons">`;
-                            for (let i in message['answers']) {
-                                let answer = message['answers'][i];
-                                html+= `
-                                <button class="chatbot-button menu-button undefined hu-color_button-text hu-background-color_button-background hu-border-color_button-background-dark"                        data-message="`+answer['goto']+`" tabindex="1"><span class="button-text">`+answer['text']+`</span></button>
-`;
-                            }
+                    <div class="hu-input-body">`;
+                        switch(message['answers_type']) {
+                            case 'button':
+                                html += `<div class="hu-input-menu"><span class="hu-input-menu-info not-selectable hu-s-10 hu-color_contrast hu-t-uppercase">Choose an option</span>
+                                        <div class="hu-input-menu-buttons">`; 
+                                for (let i in message['answers']) {
+                                    let answer = message['answers'][i];
+                                    html+= `
+                                            <button class="chatbot-button menu-button undefined hu-color_button-text hu-background-color_button-background hu-border-color_button-background-dark"                        data-message="`+answer['goto']+`" tabindex="1"><span class="button-text">`+answer['text']+`</span></button>
+                                    `;
+                                }
+                                break;
+                            case 'input':
+                                for (let i in message['answers']) {
+                                    let answer = message['answers'][i];
+                                    html += `
+                                    <div class="hu-textarea-light empty">
+                                        <div class="hu-textarea-input-container hu-background-color_textarea-background text">
+                                            <div class="hu-textarea hu-color_textarea-text" contenteditable="true" tabindex="1" placeholder="`+answer['text']+`" suppresscontenteditablewarning="true"></div>
+                                            <div class="hu-textarea-buttons">
+                                                <button class="send send-input" tabindex="2" data-name="`+answer['name']+`" data-message="`+answer['goto']+`">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" x="3650" y="3688">
+                                                        <path fill="" d="M1.1 21.757l22.7-9.73L1.1 2.3l.012 7.912 13.623 1.816-13.623 1.817-.01 7.912z"></path>
+                                                    </svg>
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <div class="separator hu-background-color_inline-input"></div>
+                                    </div>
+                                    <span class="hu-textarea-footer-info not-selectable hu-s-10 hu-color_contrast hu-t-uppercase">Press enter to send</span>
+                                    `;
+                                }
+                                break;
+                        }
                             html += `
                             </div>
                         </div>
@@ -146,6 +180,43 @@ function Chatbot(item) {
             self.currentMessage = messageNumber;
             self.writeMessage();
         });
+        self.item.on('click', '.send-input', function() {
+            let name = $(this).data('name');
+            let value = $(this).closest('.hu-textarea-input-container').find('.hu-textarea').text();
+            let messageNumber = $(this).data('message');
+            switch (name) {
+                case 'username':
+                    if (value !== '') {
+                        self[name] = value;
+                        self.writeAnswer(value);
+                        self.currentMessage = messageNumber;
+                        self.writeMessage();
+                    } 
+                    break;
+                case 'useremail':
+                    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                    if (re.test(value)) {
+                        self[name] = value;
+                        self.writeAnswer(value);
+                        self.currentMessage = messageNumber;
+                        self.writeMessage();
+                    } else {
+                        self.currentMessage = 6;
+                        self.writeMessage();
+                    }
+                    break;
+                case 'userphone':
+                    if (value !== '') {
+                        self[name] = value;
+                        self.writeAnswer(value);
+                        self.currentMessage = messageNumber;
+                        self.writeMessage();
+                    }
+                    break;
+            }
+            $(this).closest('#hu-message-input').hide();
+        });
+        
     };
     self.init = function() {
         $.getJSON("data.json", function(json) {
